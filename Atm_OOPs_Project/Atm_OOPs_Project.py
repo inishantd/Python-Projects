0b0675bdfdcd97d665942674c9868ab12237e977
Nishant Deshmukh <110892133+inishantd@users.noreply.github.com>
2022-10-25 14:01:11 +0530

    Create Atm_OOPs_Project.py

---
+++ Atm_OOPs_Project/Atm_OOPs_Project.py
@@ -0,0 +1,62 @@
+class Atm:
+    
+    #Constructor
+    def __init__(self):
+
+        self.pin = ""
+        self.balance = 0
+
+        self.menu()
+    
+    def menu(self):
+        user_input = input(
+            '''
+                Hello, how would you like to proceed?
+                1>Enter 1 to create pin.
+                2>Enter 2 to diposite.
+                3>Enter 3 to withdraw.
+                4>Enter 4 to check balance.
+                5>Enter 5 to exit.
+            ''')
+        if user_input == '1':
+            self.create_pin()
+        elif user_input == '2':
+            self.diposite()
+        elif user_input == '3':
+            self.withdraw()
+        elif user_input == '4':
+            self.check_balance()
+        else:
+            print("Bye")
+    
+    def create_pin(self):
+        self.pin = input("Enter your pin: ")
+        print("Pin set successfully")
+    
+    def diposite(self):
+        temp = input("Enter your pin: ")
+        if temp == self.pin:
+            amount = int(input("Enter the Amount: "))
+            self.balance = self.balance + amount
+            print("Deposit successful")
+        else:
+            print("Invalid Pin")
+    
+    def withdraw(self):
+        temp = input("ENter you pin: ")
+        if temp == self.pin:
+            amount = int(input("Enter the amount: "))
+            if amount < self.balance:
+                self.balance = self.balance - amount
+                print("Withdraw successful")
+            else:
+                print("Insufficient Funds")
+        else:
+            print("Invalid Pin")
+    
+    def check_balance(self):
+        temp = input("Enter your pin: ")
+        if temp == self.pin:
+            print(self.balance)
+        else:
+            print("Invalid Pin")
